eeb0150e9b1a44d99327f3eda27bc71446f12615
noorsatar78 <48452266+noorsatar78@users.noreply.github.com>
2019-03-22 13:28:07 +0300

    Add files via upload

---
+++ emoje.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 12:33:48 2019
+
+@author: Apple
+"""
+
+import turtle
+lion=turtle.Turtle()
+lion.up()
+lion.goto(0, -100)  
+lion.down()
+
+lion.begin_fill()
+lion.fillcolor("yellow")  
+lion.circle(100)
+lion.end_fill()
+
+lion.up()
+lion.goto(-67, -40)
+lion.setheading(-60)
+lion.width(5)
+lion.down()
+lion.circle(80, 120)    
+lion.fillcolor("black")
+
+for i in range(-35, 105, 70):
+    lion.up()
+    lion.goto(i, 35)
+    lion.setheading(0)
+    lion.down()
+    lion.begin_fill()
+    lion.circle(10)   
+    lion.end_fill()
+    
+lion.hideturtle()
---
+++ emojii.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 12:45:20 2019
+
+@author: Apple
+"""
+
+import emoji
+print(emoji.emojize(':kissing_heart:'))
