d85b45ae8ccb4ae82a4976f3a7bf07d170678026
noorsatar78 <48452266+noorsatar78@users.noreply.github.com>
2019-03-22 15:55:40 +0300

    Add files via upload

---
+++ flower2.py
@@ -0,0 +1,24 @@
+import turtle
+def draw_square(few_turtle):
+    for i in range(4):
+        few_turtle.forward(200)
+        few_turtle.right(90)
+def draw_mosaique_art():
+    window =turtle.Screen()
+    window.bgcolor("black")
+
+    mosaique=turtle.Turtle()
+    mosaique.shape('circle')
+    mosaique.speed('fastest')
+    mosaique.shapesize(0.5,0.5)
+    mosaique.color("red","purple")
+    mosaique.begin_fill()
+
+    for i in range (72):
+        draw_square(mosaique)
+        mosaique.right(5)
+    mosaique.end_fill
+    mosaique.forward(330)
+    window.exitonclick()
+
+draw_mosaique_art()
