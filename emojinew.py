6d28886e1ba71d6331237fd408bd021e032b3615
noorsatar78 <48452266+noorsatar78@users.noreply.github.com>
2019-03-22 21:35:02 +0300

    Add files via upload

---
+++ emojinew.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 12:45:20 2019
+
+@author: Apple
+"""
+
+import emoji
+print(emoji.emojize(":eyes:"))
+print(emoji.emojize("\U0001F600"))
+print(emoji.emojize("\U0001F911"))
+print(emoji.emojize("\U0001F61C"))
+print(emoji.emojize("\U0001F603"))
+print(emoji.emojize("\U0001F923"))
